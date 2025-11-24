2be45411a3539719d5daa983e21fb5a82f34a48c
Tan Hui Min <thmin0921@gmail.com>
2025-11-25 03:30:39 +0800

    Initial Angular resume site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {Home} from './pages/home/home';
+import {Hobbies} from './pages/hobbies/hobbies';
+import {Contact} from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'hobbies', component: Hobbies },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' } // unknown path → Home
+];
---
+++ src/app/pages/hobbies/hobbies.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hobbies',
+  imports: [],
+  templateUrl: './hobbies.html',
+  styleUrl: './hobbies.css',
+})
+export class Hobbies {
+
+}
