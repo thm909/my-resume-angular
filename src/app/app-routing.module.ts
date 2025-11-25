a7cceca9ceb16b783124cc1686d4afcd0a0ba84f
Tan Hui Min <thmin0921@gmail.com>
2025-11-25 15:34:26 +0800

    Initial Angular resume site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Hobbies } from './pages/hobbies/hobbies';
 import { Contact } from './pages/contact/contact';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 const routes: Routes = [
   { path: '', component: Home },
   { path: 'hobbies', component: Hobbies },
@@ -12,8 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })], // ✅ 使用 hash 路由
-  exports: [RouterModule],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
