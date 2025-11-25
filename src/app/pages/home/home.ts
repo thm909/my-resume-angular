94a7345ee0a5000c5863550ac0dbd2404779317b
Tan Hui Min <thmin0921@gmail.com>
2025-11-25 13:41:42 +0800

    Update latest Angular code

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [NgOptimizedImage],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
